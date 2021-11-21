fdf513f5ff522cb732e44b3c29bc43b5306c5b6a
Emre İmamoğlu <imamogluemree@gmail.com>
2021-11-21 10:08:22 +0300

    Initial commit

--- file.js
+++ file.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Yarıcap : ", function saveInput(r) {
+rl.question("Yarıcap : ", function calculate(r) {
   console.log(`Alan ${3.14 * r * r}`);
   rl.close();
 });
